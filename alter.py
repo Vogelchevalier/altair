e4a564806b61324fa73ecbb7c61d9eca4a836a49
Vogelchevalier <lappalainen.sami@outlook.com>
2020-05-26 00:18:47 +0300

    Custom error pages for 403, 404 and 500

--- alter.py
+++ alter.py
@@ -33,5 +33,20 @@ def text():
     return render_template("text.html")
 
 
+@app.errorhandler(403)
+def not_found(e):
+    return render_template('errors/403.html'), 403
+
+
+@app.errorhandler(404)
+def forbidden(e):
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('errors/500.html'), 500
+
+
 if __name__ == "__main__":
     app.run(debug=True)
