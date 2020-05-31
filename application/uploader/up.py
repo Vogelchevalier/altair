f919bce522b64e79ab585e203dd965f71ae037cb
Vogelchevalier <lappalainen.sami@outlook.com>
2020-06-01 01:37:00 +0300

    Cleaned up uploader code

--- application/uploader/up.py
+++ application/uploader/up.py
@@ -3,7 +3,7 @@
 # Under GPLv3. See LICENSE in application/uploader
 
 import os
-import secrets
+import uuid
 from datetime import datetime
 from flask import request
 from PIL import Image
@@ -35,13 +35,14 @@ def upload():
             file_without_exif = Image.new(image.mode, image.size)
             file_without_exif.putdata(data)
 
-            salt = secrets.token_urlsafe(5)
+            salt = str(uuid.uuid4()).replace("-", "")[:5]
             filename = "".join([day_time, "-", salt, extension])
+            save_path = os.path.join(root_folder, month_folder)
 
-            if not os.path.exists(root_folder + "/" + month_folder):
-                os.makedirs("".join([root_folder, "/", month_folder]), exist_ok=True)
+            if not os.path.exists(save_path):
+                os.makedirs(save_path, exist_ok=True)
 
-            file_without_exif.save(os.path.join(root_folder + "/" + month_folder, filename))
+            file_without_exif.save(os.path.join(save_path, filename))
             return "".join([domain, "/i/", month_folder, "/", filename]), 200
     else:
         return "Method not allowed", 405
